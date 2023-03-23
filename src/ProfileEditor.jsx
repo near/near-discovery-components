2d08bb25d66c54fa9275a153db35076691b09e80
Caleb Jacob <thecalebjacob@gmail.com>
2023-03-23 08:48:55 -0600

    Add ProfileEditor to adminalpha

---
+++ src/ProfileEditor.jsx
@@ -0,0 +1,12 @@
+const Wrapper = styled.div`
+  #pills-tab,
+  #pills-tabContent {
+    display: none;
+  }
+`;
+
+return (
+  <Wrapper>
+    <Widget src="mob.near/widget/ProfileEditor" />
+  </Wrapper>
+);

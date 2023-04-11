9bfcd090fc8f404630af66f62aba27fc674322b0
Michael Peter <hi@mpeter.us>
2023-04-11 17:47:06 -0400

    add flag button to posts and comments (#24)

---
+++ src/FlagButton.jsx
@@ -0,0 +1,60 @@
+const { item } = props;
+
+const Button = styled.button`
+  border: 0;
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
+  color: #687076;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  cursor: pointer;
+  background: none;
+  padding: 6px;
+  transition: color 200ms;
+
+  i {
+    font-size: 18px;
+    transition: color 200ms;
+  }
+
+  &:hover,
+  &:focus {
+    outline: none;
+    color: #11181c;
+  }
+`;
+
+return (
+  <OverlayTrigger
+    placement="top"
+    overlay={<Tooltip>Flag for moderation</Tooltip>}
+  >
+    <Button
+      type="button"
+      aria-label="Flag for moderation"
+      disabled={!context.accountId}
+      onClick={() => {
+        Social.set(
+          {
+            index: {
+              flag: JSON.stringify({
+                key: "main",
+                value: item,
+              }),
+            },
+          },
+          {
+            // onCommit: () => {
+            //TODO kick off refresh or hide the item once we have
+            // updated the feed to filter flagged items
+            // },
+          }
+        );
+      }}
+    >
+      <i className="bi bi-flag"></i>
+    </Button>
+  </OverlayTrigger>
+);

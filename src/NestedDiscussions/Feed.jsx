cae769dc94281e08fa54bf12f3a157875829ece5
gagdiez <gagdiez.c@gmail.com>
2023-04-20 16:56:44 +0200

    Implemented Nested Discussions
    NestedDiscussions allows to implement discussions anywhere by just
    importing the widget and setting an "identifier", thus allowing to
    easily have multiple types of discussions using one single widget.
    
    It uses custom notifications to notify the users if they were tagged,
    or if anyone commented or liked their discussion-post.

---
+++ src/NestedDiscussions/Feed.jsx
@@ -0,0 +1,45 @@
+const dbAction = props.dbAction;
+const composeWidget = props.composeWidget;
+const previewWidget = props.previewWidget;
+const identifier = props.identifier;
+const moderatorAccount = props.moderatorAccount;
+
+const index = {
+  action: dbAction,
+  key: identifier,
+  options: { subscribe: true },
+};
+
+const Post = styled.div`
+  padding: 24px 0 12px;
+
+  @media (max-width: 1200px) {
+    padding: 12px 0 0;
+  }
+`;
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <Post className="post" key={JSON.stringify(a)}>
+      <Widget
+        src={previewWidget}
+        props={{
+          accountId: a.accountId,
+          blockHeight: a.blockHeight,
+          dbAction,
+          composeWidget,
+          previewWidget,
+          moderatorAccount,
+        }}
+      />
+    </Post>
+  );
+
+return (
+  <>
+    <Widget
+      src="adminalpha.near/widget/IndexFeed"
+      props={{ index, renderItem, moderatorAccount, reverse: true }}
+    />
+  </>
+);
---
+++ src/NestedDiscussions/Preview.jsx
@@ -0,0 +1,185 @@
+// Base case, they called a preview on a discussion, we simply return the main widget
+if (typeof props.identifier === "string") {
+  return (
+    <Widget
+      src="near/widget/NestedDiscussions"
+      props={{ identifier: props.identifier }}
+    />
+  );
+}
+
+// Otherwise, they want to preview a specific comment
+const dbAction = props.dbAction;
+const accountId = props.accountId;
+const blockHeight = parseInt(props.blockHeight);
+
+const composeWidget = "near/widget/NestedDiscussions.Compose";
+const previewWidget = "near/widget/NestedDiscussions.Preview";
+
+const content = JSON.parse(
+  Social.get(`${accountId}/${dbAction}/main`, blockHeight)
+).content;
+
+const postUrl = `https://alpha.near.org/#/${previewWidget}?accountId=${accountId}&blockHeight=${blockHeight}&dbAction=${dbAction}`;
+
+// all children comments will be identified by this object
+const item = {
+  accountId,
+  blockHeight,
+  dbAction,
+};
+
+const Post = styled.div`
+    position: relative;
+  
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      left: 19px;
+      top: 52px;
+      bottom: 12px;
+      width: 2px;
+      background: #ECEEF0;
+    }
+  `;
+
+const Header = styled.div`
+    margin-bottom: 0;
+    display: inline-flex;
+  `;
+
+const Body = styled.div`
+    padding-left: 52px;
+    padding-bottom: 1px;
+  `;
+
+const Content = styled.div`
+    img {
+      display: block;
+      max-width: 100%;
+      max-height: 80vh;
+      margin: 0 0 12px;
+    }
+  `;
+
+const Text = styled.p`
+    display: block;
+    margin: 0;
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 400;
+    color: #687076;
+    white-space: nowrap;
+  `;
+
+const Actions = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    margin: -6px -6px 6px;
+  `;
+
+const Comments = styled.div`
+    > div > div:first-child {
+      padding-top: 12px;
+    }
+  `;
+
+return (
+  <Post>
+    <Header>
+      <Widget
+        src="calebjacob.near/widget/AccountProfile"
+        props={{
+          accountId,
+          hideAccountId: true,
+          inlineContent: (
+            <>
+              <Text as="span">･</Text>
+              <Text>
+                <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />{" "}
+                ago
+              </Text>
+            </>
+          ),
+        }}
+      />
+    </Header>
+
+    <Body>
+      <Content>
+        {content.text && (
+          <Widget
+            src="calebjacob.near/widget/SocialMarkdown"
+            props={{ text: content.text }}
+          />
+        )}
+
+        {content.image && (
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: content.image,
+            }}
+          />
+        )}
+      </Content>
+
+      {blockHeight !== "now" && (
+        <Actions>
+          <Widget
+            src="near/widget/NestedDiscussions.Preview.LikeButton"
+            props={{
+              item,
+              previewWidget,
+              notifyAccountId: accountId,
+            }}
+          />
+          <Widget
+            src="near/widget/NestedDiscussions.Preview.CommentButton"
+            props={{
+              dbAction,
+              item,
+              onClick: () => State.update({ showReply: !state.showReply }),
+            }}
+          />
+          <Widget
+            src="calebjacob.near/widget/CopyUrlButton"
+            props={{
+              url: postUrl,
+            }}
+          />
+        </Actions>
+      )}
+
+      {state.showReply && (
+        <div className="mb-2">
+          <Widget
+            src={composeWidget}
+            props={{
+              dbAction,
+              notifyAccountId: accountId,
+              previewWidget,
+              identifier: item,
+              onComment: () => State.update({ showReply: false }),
+            }}
+          />
+        </div>
+      )}
+
+      <Comments>
+        <Widget
+          src="near/widget/NestedDiscussions.Feed"
+          props={{
+            dbAction,
+            identifier: item,
+            composeWidget,
+            previewWidget,
+            moderatorAccount,
+          }}
+        />
+      </Comments>
+    </Body>
+  </Post>
+);

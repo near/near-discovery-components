1eb89cb72c53404525cbed5a5fd6cd4e7db59be7
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-04-21 20:58:47 +0300

    Add Onboarding.Starter component (#62)

---
+++ src/Onboarding/Starter.jsx
@@ -0,0 +1,9 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget />
+    <br />
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget />
+  </>
+);

4f7b8f9954ceac4643349bd70cbf37d93b5b3ea7
Caleb Jacob <thecalebjacob@gmail.com>
2023-04-24 09:39:13 -0600

    Fixing more /#/ urls by switching to #/ to avoid page reload

--- src/ComponentCard.jsx
+++ src/ComponentCard.jsx
@@ -5,7 +5,7 @@ const metadata = Social.get(
 );
 const tags = Object.keys(metadata.tags || {});
 const detailsUrl = `#/near/widget/ComponentDetailsPage?src=${accountId}/widget/${widgetName}`;
-const appUrl = `/#/${accountId}/widget/${widgetName}`;
+const appUrl = `#/${accountId}/widget/${widgetName}`;
 const accountUrl = `#/near/widget/ProfilePage?accountId=${accountId}`;
 
 const Card = styled.div`

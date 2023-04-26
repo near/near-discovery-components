c4f2ca014622eaa0c8e9e6e05e7e1b431e1c9bcb
Phuong Nguyen <phuong@near.org>
2023-04-26 16:26:43 -0700

    Fix typeahead markdown

--- src/Search/Markdown.jsx
+++ src/Search/Markdown.jsx
@@ -1,18 +1,10 @@
 const Wrapper = styled.div`
-  color: #606D7A !important;
-  font-size: 14px !important;
-  font-weight: 400 !important;
-  line-height: 20 !important;
-  text-align: left !important;
-  margin: 0 !important;
-  padding: 0 !important;
-  /* add the remaining properties */
+  font-size: 12px;
+  line-height: 20px;
+  font-weight: 400;
   color: #606d7a;
+  text-align: left;
   word-break: break-word;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
 
   > * {
     margin-bottom: 12px;
@@ -27,12 +19,10 @@ const Wrapper = styled.div`
     font-weight: 400;
     font-size: 12px;
     line-height: 1.4em;
-    color: #606D7A;
+    color: #606d7a;
   }
 
-  h1 {
-    font-size: 12px;
-  }
+  h1,
   h2 {
     font-size: 12px;
   }
@@ -42,21 +32,15 @@ const Wrapper = styled.div`
   }
 
   a {
-    color: #006adc;
+    color: #606d7a;
     outline: none;
-    font-weight: 600;
+    font-weight: 400;
 
     &:hover,
     &:focus {
-      color: #006adc;
-      text-decoration: underline;
+      color: #606d7a;
+      text-decoration: none;
     }
-  }
-
-  b {
-    font-weight: 700;
-    text-decoration: underline;
-
   }
 
   img {
@@ -66,25 +50,8 @@ const Wrapper = styled.div`
   }
 `;
 
-
-
-const renderMention =
-  props.renderMention ??
-  ((accountId) => (
-    <Widget
-      key={accountId}
-      src="near/widget/AccountProfileInline"
-      props={{
-        accountId,
-        hideAvatar: true,
-      }}
-    />
-  ));
-
-const word = () => {};
-
 return (
-  <Wrapper style={{ fontSize: "12px", color:'#606D7A'}}>
-    <Markdown  text={props.text} onMention={renderMention} />
+  <Wrapper>
+    <Markdown text={props.text} />
   </Wrapper>
 );

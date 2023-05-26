f34d78fc3066454934522d0d99f4d187c91de631
Dmitriy <sheleg.dmitriy@gmail.com>
2023-05-26 20:20:11 +0300

    Add missing type declarations

--- vm.d.ts
+++ vm.d.ts
@@ -18,13 +18,16 @@ declare namespace State {
    */
   function update(state: object, init?: object): void;
 }
+
 type BlockIdentity = 'final' | number;
+type NearBlockIdentity = BlockIdentity | 'optimistic';
 type SocialReturnType = 'History' | 'True' | 'BlockHeight';
-type NetworkId = 'mainnet' | 'testnet'
+type NetworkId = 'mainnet' | 'testnet';
+
 /**
  * @param subscribe (optional) if true, the data will be refreshed every 5 seconds.
  * @param return_deleted (optional) whether to return deleted values (as `null`). Default is `false`.
-*/
+ */
 interface SocialGetOptions {
   subscribe?: boolean;
   return_deleted?: boolean;
@@ -39,7 +42,7 @@ interface SocialGetOptions {
 type SocialKeysOptions = SocialGetOptions & {
   return_type?: SocialReturnType;
   values_only?: boolean;
-}
+};
 
 /**
  * @param subscribe (optional) if `true`, the data will be refreshed every 5 seconds.
@@ -69,7 +72,7 @@ interface SocialSetOptions {
 
 declare namespace Social {
   /**
-   * `Social.get` fetches the data from the SocialDB contract by calling get and returns the data. While the data is fetching the returned value equals to `null`.
+   * `Social.get` fetches the data from the SocialDB contract by calling get and returns the data. While the data is fetching, the returned value equals to `null`.
    * More info: {@link https://docs.near.org/bos/api/social#socialget}
    * @param patterns the path pattern(s);
    * @param finality (optional) the block height or finality;
@@ -78,7 +81,7 @@ declare namespace Social {
   function get(patterns: string | string[], finality?: BlockIdentity, options?: SocialGetOptions): void;
 
   /**
-   * `Social.getr` is just a wrapper helper for {@link Social.get}, it appends `**` to each of the path pattern.
+   * `Social.getr` is just a wrapper helper for {@link Social.get}, it appends `**` to each of the path patterns.
    * More info: {@link https://docs.near.org/bos/api/social#socialgetr}
    * @param patterns the path pattern(s);
    * @param finality (optional) the block height or finality;
@@ -87,7 +90,7 @@ declare namespace Social {
   function getr(patterns: string | string[], finality?: BlockIdentity, options?: SocialGetOptions): void;
 
   /**
-   * It calls the SocialDB's `keys` API and returns the data. While the data is fetching the returned value equals to `null`. The keys contract doesn't unwrap the object, so the returned data is the same as the SocialDB's `keys` API.
+   * It calls the SocialDB's `keys` API and returns the data. While the data is fetching, the returned value equals to `null`. The keys contract doesn't unwrap the object, so the returned data is the same as the SocialDB's `keys` API.
    * More info: {@link https://docs.near.org/bos/api/social#socialkeys}
    * @param patterns the path pattern(s);
    * @param finality (optional) the block height or finality;
@@ -105,7 +108,7 @@ declare namespace Social {
   function index(action: string, key: string, options?: SocialIndexOptions): void;
 
   /**
-   * Takes a `data` object and commits it to SocialDB. It works similarly to the `CommitButton` by spawning the modal window prompt to save data, but it doesn't have to be triggered through the commit button component. It allows you to write more flexible code that relies on async promises and use other events and components. Overall it enables more flexibility when committing to SocialDB. For example, you can commit when Enter key pressed.
+   * Takes a `data` object and commits it to SocialDB. It works similarly to the `CommitButton` by spawning the modal window prompt to save data, but it doesn't have to be triggered through the commit button component. It allows you to write more flexible code that relies on async promises and use other events and components. Overall, it enables more flexibility when committing to SocialDB. For example, you can commit when the Enter key is pressed.
    * More info: {@link https://docs.near.org/bos/api/social#socialset}
    * @param data the data object to be committed. Similar to `CommitButton`, it shouldn't start with an account ID.
    * @param options {@link SocialSetOptions | optional} object.
@@ -123,4 +126,84 @@ declare namespace context {
    */
   const networkId: NetworkId;
   const accountId: string;
+}
+
+/**
+ * VM provides a convenient API to interact with the NEAR blockchain.
+ * More info {@link https://docs.near.org/bos/api/near}
+ */
+declare namespace Near {
+  /**
+   * The block height (`number`) or finality level to use for the blockchain query (desired block height, or one of the following strings: `optimistic`, `final`)
+   * @param blockHeightOrFinality `number`, `optimistic` or `final`.
+   */
+  function block(blockHeightOrFinality?: NearBlockIdentity): void;
+
+  /**
+   * This will conduct a call to a smart contract that will store a message onchain.
+   * @param contractName name of the smart contract to call.
+   * @param methodName name of the method to call on the smart contract.
+   * @param args (optional) arguments to pass to the smart contract method as an object instance.
+   * @param gas (optional) maximum amount of gas to be used for the transaction (default 300Tg)
+   * @param deposit (optional) amount of NEAR tokens to attach to the call as deposit (in yoctoNEAR units).
+   */
+  function call(contractName: string, methodName: string, args?: object, gas?: string | number, deposit?: string | number): void;
+
+  /**
+   * This will make a view call to a smart contract that will get the message from blockchain.
+   * @param contractName name of the smart contract to call.
+   * @param methodName name of the method to call on the smart contract.
+   * @param args (optional) arguments to pass to the smart contract method as an object instance.
+   * @param blockId block ID or finality of the transaction.
+   * @param subscribe this feature allows users to subscribe to a query, which automatically refreshes the data for all subscribers every 5 seconds.
+   */
+  function view(contractName: string, methodName: string, args?: object, blockId?: string, subscribe?: boolean): void;
+}
+
+/**
+ * The VM implements a clipboard API that works like {@link https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText | Mozilla's Clipboard}, providing write access to the contents of the system clipboard. The Clipboard API can be used to implement cut, copy, and paste features within a web application.
+ * More info {@link https://docs.near.org/bos/api/clipboard}
+ */
+declare namespace clipboard {
+  /**
+   * Copies the specified text string to the system clipboard.
+   * @param text data to be copied to the clipboard.
+   */
+  function writeText(text: string): void;
+}
+
+/**
+ * `Storage` object to store data for components that is persistent across refreshes. Simulates `localStorage` access.
+ */
+declare namespace StorageAPI {
+  interface Storage {
+    /**
+     * `Storage.get` - returns the public value for a given `key` under the given `widgetSrc` or the current component if `widgetSrc` is omitted. Can only read public values.
+     * More info {@link https://docs.near.org/bos/api/storage#storageget}
+     * @param key a user-defined key.
+     * @param widgetSrc (optional) a user-defined component.
+     */
+    get(key: string, widgetSrc?: string): Promise<string | null>;
+
+    /**
+     * `Storage.set` - sets the public value for a given key under the current widget. The value will be public, so other widgets can read it.
+     * More info {@link https://docs.near.org/bos/api/storage#storageset}
+     * @param key a user-defined key.
+     * @param value a user-defined value.
+     */
+    set(key: string, value: string): Promise<void>;
+
+    /**
+     * `Storage.privateGet` - returns the private value for a given key under the current component.
+     * @param key a user-defined key under the current component.
+     */
+    privateGet(key: string): Promise<string | null>;
+
+    /**
+     * `Storage.privateSet` - sets the private value for a given key under the current component. The value is private, only the current component can read it.
+     * @param key a user-defined key under the current component.
+     * @param value a user-defined value
+     */
+    privateSet(key: string, value: string): Promise<void>;
+  }
 }

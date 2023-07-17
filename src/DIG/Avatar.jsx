23075df883d802dfac7b57b4e964637dcce64d31
Caleb Jacob <thecalebjacob@gmail.com>
2023-07-17 13:04:53 -0600

    Implement DIG.Avatar

---
+++ src/DIG/Avatar.jsx
@@ -0,0 +1,44 @@
+let { alt, image, size } = props;
+
+alt = alt ?? "User avatar";
+size = size ?? "small";
+
+function returnSize() {
+  switch (size) {
+    case "medium":
+      return "38px";
+    case "small":
+      return "28px";
+  }
+
+  return size;
+}
+
+const Avatar = styled.div`
+  width: ${returnSize()};
+  height: ${returnSize()};
+  flex-shrink: 0;
+  overflow: hidden;
+  border-radius: 100%;
+
+  img {
+    border: none;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+return (
+  <Avatar>
+    <Widget
+      src="${REPL_MOB}/widget/Image"
+      props={{
+        image,
+        alt,
+        fallbackUrl:
+          "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
+      }}
+    />
+  </Avatar>
+);

53e469f59be24dd79c62ec22b9481fb6ce255601
Caleb Jacob <thecalebjacob@gmail.com>
2023-08-14 12:09:14 -0600

    Fixing search icon

--- src/DIG/InputSearch.jsx
+++ src/DIG/InputSearch.jsx
@@ -21,7 +21,25 @@ function reset() {
 
 const ResetButton = styled.button`
   all: unset;
-  display: block;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1.5rem;
+  width: 1.5rem;
+  border-radius: 100%;
+  background: var(--sand5);
+  text-align: center;
+  transition: all 200ms;
+  margin-right: -0.25rem;
+
+  i {
+    font-size: 12px !important;
+    color: var(--sand12) !important;
+  }
+
+  &:hover {
+    background: var(--sand6);
+  }
 
   input:placeholder-shown + & {
     display: none;
@@ -32,10 +50,15 @@ return (
   <Widget
     src="${REPL_ACCOUNT}/widget/DIG.Input"
     props={{
-      iconLeft: "ph-bold ph-search",
+      iconLeft: "ph-bold ph-magnifying-glass",
       inputNodeRight: (
-        <ResetButton type="button" tabindex={-1} onClick={reset}>
-          <span className="ph-bold ph-x" />
+        <ResetButton
+          type="button"
+          tabIndex={-1}
+          onClick={reset}
+          aria-label="Reset Input"
+        >
+          <i className="ph-bold ph-x" />
         </ResetButton>
       ),
       onInput: handleOnInput,

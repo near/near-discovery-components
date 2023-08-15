6b519808896a0fecb785ae1effaee62a87788473
Michael Peter <m.peter.dev@gmail.com>
2023-08-15 11:47:45 -0400

    WIP

--- src/Posts/Post.jsx
+++ src/Posts/Post.jsx
@@ -311,6 +311,12 @@ return (
             }}
           />
           <Widget
+            src="${REPL_ACCOUNT}/widget/Posts.RepostButton"
+            props={{
+              item,
+            }}
+          />
+          <Widget
             src="${REPL_ACCOUNT}/widget/CopyUrlButton"
             props={{
               url: postUrl,

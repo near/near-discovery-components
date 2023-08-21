3246491beb30a2dc4c6087c986cccc88e34dd337
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-08-21 10:22:25 +0300

    Implement DIG.Dialog (#191)

---
+++ src/DIG/Dialog.jsx
@@ -0,0 +1,173 @@
+let {
+  type,
+  title,
+  description,
+  onCancel,
+  onConfirm,
+  cancelButtonText,
+  confirmButtonText,
+  cancelButton,
+  confirmButton,
+  ...forwardedProps
+} = props;
+
+type = type ?? "dialog";
+
+if (["alert", "dialog"].indexOf(type) < 0) {
+  return "Unsupported type of component. `type` could be only 'alert' or 'dialog'";
+}
+
+const variant = type === "alert" ? "AlertDialog" : "Dialog";
+
+const Root = styled(`${variant}.Root`)``;
+
+const Overlay = styled(`${variant}.Overlay`)`
+  background-color: var(--blackA3);
+  position: fixed;
+  inset: 0;
+  animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+
+  @keyframes overlayShow {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const Content = styled(`${variant}.Content`)`
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: 0px 4px 8px 0px var(--blackA3), 0px 0px 0px 1px var(--blackA4);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  max-width: 409px;
+  max-height: 85vh;
+  padding: 24px;
+  animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+
+  @keyframes contentShow {
+    from {
+      opacity: 0;
+      transform: translate(-50%, -48%) scale(0.96);
+    }
+    to {
+      opacity: 1;
+      transform: translate(-50%, -50%) scale(1);
+    }
+  }
+`;
+
+const Title = styled(`${variant}.Title`)`
+  font: var(--text-base);
+  font-weight: 700;
+  color: var(--sand12);
+`;
+
+const Description = styled(`${variant}.Description`)`
+  font: var(--text-s);
+  color: var(--sand11);
+`;
+
+const ActionWrapper = styled.div`
+  display: flex;
+  gap: 12px;
+  justify-content: flex-end;
+`;
+
+const Close = styled.button`
+  all: unset;
+  font-family: inherit;
+  border-radius: 100%;
+  height: 25px;
+  width: 25px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--violet11);
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  transition: all 200ms;
+  cursor: pointer;
+  border: 1px solid transparent;
+
+  &:hover {
+    border-color: var(--violet6);
+    color: var(--violet8);
+  }
+
+  &:focus-within {
+    box-shadow: 0 0 0 4px var(--violet4);
+  }
+`;
+
+cancelButton = cancelButton ?? (
+  <Widget
+    src="${REPL_ACCOUNT}/widget/DIG.Button"
+    props={{
+      label: cancelButtonText ?? "Cancel",
+      variant: "secondary",
+    }}
+  />
+);
+
+confirmButton = confirmButton ?? (
+  <Widget
+    src="${REPL_ACCOUNT}/widget/DIG.Button"
+    props={{
+      label: confirmButtonText ?? "Confirm",
+      variant: "primary",
+    }}
+  />
+);
+
+return (
+  <Root {...forwardedProps}>
+    {type === "alert" ? (
+      <AlertDialog.Trigger asChild>{trigger}</AlertDialog.Trigger>
+    ) : (
+      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
+    )}
+    <Overlay />
+    <Content>
+      {title && <Title>{title}</Title>}
+      {description && <Description>{description}</Description>}
+      <ActionWrapper>
+        {type === "alert" ? (
+          <AlertDialog.Cancel
+            asChild
+            onClick={!cancelButton ? () => {} : onCancel}
+          >
+            {cancelButton}
+          </AlertDialog.Cancel>
+        ) : (
+          <Dialog.Close asChild onClick={!confirmButton ? () => {} : onConfirm}>
+            {confirmButton}
+          </Dialog.Close>
+        )}
+
+        {type === "alert" && (
+          <AlertDialog.Action
+            asChild
+            onClick={!confirmButton ? () => {} : onConfirm}
+          >
+            {confirmButton}
+          </AlertDialog.Action>
+        )}
+      </ActionWrapper>
+      {type === "dialog" && (
+        <Dialog.Close asChild>
+          <Close>
+            <i class="ph ph-x" />
+          </Close>
+        </Dialog.Close>
+      )}
+    </Content>
+  </Root>
+);

f292c86518af900ad9c065153250688abeab67e5
Gabe Hamilton <gabe@near.org>
2023-10-05 13:51:45 -0600

    Formatting

--- src/Moderation/ModerateAccounts.jsx
+++ src/Moderation/ModerateAccounts.jsx
@@ -4,97 +4,112 @@ const moderatorAccount = props?.moderatorAccount || "${REPL_MODERATOR}";
 const moderationStream = props.moderationStream || moderatorAccount;
 
 State.init({
-    inputContent: ""
+  inputContent: "",
 });
 
 function blockAccounts(input) {
-    let accountIds = input.split(",").map((a) => a.trim());
+  let accountIds = input.split(",").map((a) => a.trim());
 
-    return accountIds.map((accountId) =>
-        ({key: moderationStream, value: {path: accountId, label: 'moderate'}}), []);
+  return accountIds.map(
+    (accountId) => ({
+      key: moderationStream,
+      value: { path: accountId, label: "moderate" },
+    }),
+    [],
+  );
 }
 
 function unblockAccount(accountId) {
-    return JSON.stringify({key: moderationStream, value: {path: accountId, label: null, operation: 'delete'}});
+  return JSON.stringify({
+    key: moderationStream,
+    value: { path: accountId, label: null, operation: "delete" },
+  });
 }
 
 const moderatedObjects = context.accountId
-    ? Social.index("moderate", moderationStream, {subscribe: true, order: 'desc'})
-        .filter((f) => f.accountId === moderatorAccount)
-    : [];
+  ? Social.index("moderate", moderationStream, {
+      subscribe: true,
+      order: "desc",
+    }).filter((f) => f.accountId === moderatorAccount)
+  : [];
 const modifiedModerations = {}; // track update & delete operations
 
 return (
-    <>
-        <div className="d-flex flex-row gap-2 mb-5">
-        <span style={{color: "grey", float: left, paddingRight: '2em'}}>
-          When saving, ensure data is being written to moderator key. You may need
-          to refresh your browser if you used "Pretend to be another account" while
-          on this page
-        </span>
-            <input
-                type="text"
-                value={state.inputContent}
-                onChange={(e) => {
-                    State.update({
-                        inputContent: e.target.value,
-                    });
-                }}
-                placeholder="Comma-separated list of accounts"
-            />
-            <CommitButton
-                force
-                data={ {index: {moderate: blockAccounts(state.inputContent)} } }
-                onCommit={() => {
-                    State.update({inputContent: ""});
-                }}
-            >
-                Add to filter
-            </CommitButton>
+  <>
+    <div className="d-flex flex-row gap-2 mb-5">
+      <span style={{ color: "grey", float: left, paddingRight: "2em" }}>
+        When saving, ensure data is being written to moderator key. You may need
+        to refresh your browser if you used "Pretend to be another account"
+        while on this page
+      </span>
+      <input
+        type="text"
+        value={state.inputContent}
+        onChange={(e) => {
+          State.update({
+            inputContent: e.target.value,
+          });
+        }}
+        placeholder="Comma-separated list of accounts"
+      />
+      <CommitButton
+        force
+        data={{ index: { moderate: blockAccounts(state.inputContent) } }}
+        onCommit={() => {
+          State.update({ inputContent: "" });
+        }}
+      >
+        Add to filter
+      </CommitButton>
+    </div>
+    <h3>Moderated (blocked) Accounts</h3>
+    <table className="table">
+      <th>Account</th>
+      <th>Reason</th>
+      <th>Clear moderation</th>
+      <tbody>
+        {moderatedObjects &&
+          moderatedObjects.map((f) => {
+            if (!f || !f.value || !f.value.path) {
+              return;
+            }
 
-        </div>
-        <h3>Moderated (blocked) Accounts</h3>
-        <table className="table">
-            <th>Account</th>
-            <th>Reason</th>
-            <th>Clear moderation</th>
-            <tbody>
-            {moderatedObjects && moderatedObjects.map((f) => {
-                if(!f || !f.value || !f.value.path) {
-                    return
-                }
+            const account = f.value.path;
+            const mod = modifiedModerations[account];
+            if (mod && mod.operation === "delete") {
+              return;
+            }
 
-                const account = f.value.path;
-                const mod = modifiedModerations[account];
-                if(mod && mod.operation === 'delete') {
-                    return;
-                }
-
-                // todo account for longer sequences of operations
-                if(f.value.operation) {
-                    modifiedModerations[account] = {operation: f.value.operation, label: f.value.label};
-                } else {
-                    const label = mod && mod.operation === 'update' ? mod.label : f.value.label;
-                    return (
-                        <tr key={account}>
-                            <td>{account}</td>
-                            <td>{label}</td>
-                            <td>
-                                <CommitButton
-                                    className="btn btn-danger"
-                                    data={{
-                                        index: {
-                                            moderate: unblockAccount(account),
-                                        }
-                                    }}
-                                >
-                                    <i className="bi bi-x"/>
-                                </CommitButton>
-                            </td>
-                        </tr>
-                    )}})}
-            </tbody>
-        </table>
-
-    </>
+            // todo account for longer sequences of operations
+            if (f.value.operation) {
+              modifiedModerations[account] = {
+                operation: f.value.operation,
+                label: f.value.label,
+              };
+            } else {
+              const label =
+                mod && mod.operation === "update" ? mod.label : f.value.label;
+              return (
+                <tr key={account}>
+                  <td>{account}</td>
+                  <td>{label}</td>
+                  <td>
+                    <CommitButton
+                      className="btn btn-danger"
+                      data={{
+                        index: {
+                          moderate: unblockAccount(account),
+                        },
+                      }}
+                    >
+                      <i className="bi bi-x" />
+                    </CommitButton>
+                  </td>
+                </tr>
+              );
+            }
+          })}
+      </tbody>
+    </table>
+  </>
 );
--- src/Moderation/Moderator.jsx
+++ src/Moderation/Moderator.jsx
@@ -2,8 +2,8 @@
 
 const Content = styled.div`
   .post {
-  padding-left: 0;
-  padding-right: 0;
+    padding-left: 0;
+    padding-right: 0;
   }
 `;
 const Tabs = styled.div`
@@ -15,13 +15,13 @@ const Tabs = styled.div`
   scroll-behavior: smooth;
 
   @media (max-width: 1024px) {
-  background: #f8f9fa;
-  border-top: 1px solid #eceef0;
-  margin: 0 -12px 48px;
+    background: #f8f9fa;
+    border-top: 1px solid #eceef0;
+    margin: 0 -12px 48px;
 
-  > * {
-    flex: 1;
-  }
+    > * {
+      flex: 1;
+    }
   }
 `;
 
@@ -42,93 +42,92 @@ const TabsButton = styled.a`
   text-decoration: none !important;
 
   &:hover {
-  color: #11181c;
+    color: #11181c;
   }
 
   &::after {
-  content: "";
-  display: ${(p) => (p.selected ? "block" : "none")};
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 3px;
-  background: #59e692;
+    content: "";
+    display: ${(p) => (p.selected ? "block" : "none")};
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 3px;
+    background: #59e692;
   }
 `;
 
 State.init({
-    selectedTab: props.tab || "reported",
+  selectedTab: props.tab || "reported",
 });
 if (props.tab && props.tab !== state.selectedTab) {
-    State.update({
-        selectedTab: props.tab,
-    });
+  State.update({
+    selectedTab: props.tab,
+  });
 }
 
 const baseUrl = `#/${REPL_ACCOUNT}/widget/Moderation.Moderator`;
 const moderatorAccount = props?.moderatorAccount || "${REPL_MODERATOR}";
 const moderationStream = props.moderationStream || moderatorAccount;
 
-return(
-    <div className="d-flex flex-column gap-5">
-        <span>Admin: {moderatorAccount}</span>
-        <Content>
-            <Tabs>
-                <TabsButton
-                    href={`${baseUrl}?&tab=reported`}
-                    selected={state.selectedTab === "reported"}
-                >
-                    Reported and needs moderation
-                </TabsButton>
+return (
+  <div className="d-flex flex-column gap-5">
+    <span>Admin: {moderatorAccount}</span>
+    <Content>
+      <Tabs>
+        <TabsButton
+          href={`${baseUrl}?&tab=reported`}
+          selected={state.selectedTab === "reported"}
+        >
+          Reported and needs moderation
+        </TabsButton>
 
-                <TabsButton
-                    href={`${baseUrl}?&tab=moderated_accounts`}
-                    selected={state.selectedTab === "moderated_accounts"}
-                >
-                    Moderated Accounts
-                </TabsButton>
+        <TabsButton
+          href={`${baseUrl}?&tab=moderated_accounts`}
+          selected={state.selectedTab === "moderated_accounts"}
+        >
+          Moderated Accounts
+        </TabsButton>
 
-                <TabsButton
-                    href={`${baseUrl}?&tab=moderated_posts`}
-                    selected={state.selectedTab === "moderated_posts"}
-                >
-                    Moderated Posts
-                </TabsButton>
+        <TabsButton
+          href={`${baseUrl}?&tab=moderated_posts`}
+          selected={state.selectedTab === "moderated_posts"}
+        >
+          Moderated Posts
+        </TabsButton>
 
-                <TabsButton
-                    href={`${baseUrl}?&tab=moderated_comments`}
-                    selected={state.selectedTab === "moderated_comments"}
-                >
-                    Moderated Comments
-                </TabsButton>
-            </Tabs>
+        <TabsButton
+          href={`${baseUrl}?&tab=moderated_comments`}
+          selected={state.selectedTab === "moderated_comments"}
+        >
+          Moderated Comments
+        </TabsButton>
+      </Tabs>
 
-            {state.selectedTab === "reported" && (
-                <Widget
-                    src="${REPL_ACCOUNT}/widget/Flagged.Feed"
-                />
-            )}
+      {state.selectedTab === "reported" && (
+        <Widget src="${REPL_ACCOUNT}/widget/Flagged.Feed" />
+      )}
 
+      {state.selectedTab === "moderated_accounts" && (
+        <Widget
+          src="${REPL_ACCOUNT}/widget/Moderation.ModerateAccounts"
+          props={{ moderatorAccount, moderationStream }}
+        />
+      )}
 
-            {state.selectedTab === "moderated_accounts" &&
-                <Widget
-                    src="${REPL_ACCOUNT}/widget/Moderation.ModerateAccounts"
-                    props={{ moderatorAccount, moderationStream, }}/>
-            }
+      {state.selectedTab === "moderated_posts" && (
+        <Widget
+          src="${REPL_ACCOUNT}/widget/Moderation.ModeratePosts"
+          props={{ moderatorAccount, moderationStream }}
+        />
+      )}
 
-            {state.selectedTab === "moderated_posts" &&
-                <Widget
-                    src="${REPL_ACCOUNT}/widget/Moderation.ModeratePosts"
-                    props={{ moderatorAccount, moderationStream, }}/>
-            }
-
-            {state.selectedTab === "moderated_comments" && (
-                <Widget
-                    src="${REPL_ACCOUNT}/widget/Moderation.ModerateComments"
-                    props={{ moderatorAccount, moderationStream, }}/>
-            )}
-
-        </Content>
-    </div>
+      {state.selectedTab === "moderated_comments" && (
+        <Widget
+          src="${REPL_ACCOUNT}/widget/Moderation.ModerateComments"
+          props={{ moderatorAccount, moderationStream }}
+        />
+      )}
+    </Content>
+  </div>
 );

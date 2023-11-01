cf7a30c5f84afabf529326c55d6d6938425b55a9
Gabe Hamilton <gabe@near.org>
2023-11-01 13:42:46 -0600

    Show Verified Badges on Profile

--- src/ProfilePage/Sidebar.jsx
+++ src/ProfilePage/Sidebar.jsx
@@ -236,6 +236,9 @@ const FollowButtonWrapper = styled.div`
   }
 `;
 
+const Verifications = styled.div`
+  padding: 0;
+`;
 return (
   <Wrapper>
     <Avatar>
@@ -335,6 +338,12 @@ return (
           </button>
         </OverlayTrigger>
       </Actions>
+      <Verifications>
+        <Widget
+            src="${REPL_ACCOUNT}/widget/ProfilePage.ProfileBadges"
+            props={{accountId}}
+        />
+      </Verifications>
     </Section>
 
     {tags.length > 0 && (

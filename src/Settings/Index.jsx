2d583962d2169fb10cb5b6ff847e229aa09561ac
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-11-06 15:34:31 +0200

    Show tab content on settings page (#490)

--- src/Settings/Index.jsx
+++ src/Settings/Index.jsx
@@ -1,21 +1,22 @@
 let { idosConnected, ...forwardedProps } = props;
 
-const activeTab = Storage.get("settings-tab");
+const activeTab = Storage.get("settings-tab") ?? "identity";
 // tab names: account, content, news, identity, notifications
 
+// Uncomment this when we have implement more then one tab.
 const Wrapper = styled.div`
-  display: grid;
-  gap: 40px;
-  grid-template-columns: 264px minmax(0, 1fr);
-  align-items: start;
-  height: 100%;
+  // display: grid;
+  // gap: 40px;
+  // grid-template-columns: 264px minmax(0, 1fr);
+  // align-items: start;
+  // height: 100%;
 
-  @media (max-width: 1024px) {
-    grid-template-columns: minmax(0, 1fr);
-  }
+  // @media (max-width: 1024px) {
+  //   grid-template-columns: minmax(0, 1fr);
+  // }
 `;
 
-const renderContent = () => {
+const SettingsContent = () => {
   switch (activeTab) {
     case "account":
     case "content":
@@ -23,9 +24,8 @@ const renderContent = () => {
     case "notifications":
       return <div>Not implemented yet</div>;
     case "identity":
+      default:
       return <Widget src="${REPL_ACCOUNT}/widget/Settings.Identity.Index" props={{ idosConnected, ...forwardedProps }} />;
-    default:
-      return null;
   }
 };
 
@@ -33,15 +33,19 @@ const handleMenuClick = (value) => {
   Storage.set("settings-tab", value);
 };
 
+// So far we have only implemented the identity tab. That's why we don't want to show Sidebar yet.
+const Sidebar = () => (
+  <Widget
+    src="${REPL_ACCOUNT}/widget/Settings.Sidebar"
+    props={{
+      onClick: handleMenuClick,
+      activeTab
+    }}
+  />
+);
+
 return (
   <Wrapper className="container-xl">
-    <Widget
-      src="${REPL_ACCOUNT}/widget/Settings.Sidebar"
-      props={{
-        onClick: handleMenuClick,
-        activeTab
-      }}
-    />
-    {renderContent()}
+    <SettingsContent />
   </Wrapper>
 );

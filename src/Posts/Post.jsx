787b5d8124b9eba8844adf4e7f618f7c4ba6fad9
Erdit Kurteshi <erdit.kurteshi@sqa-consulting.com>
2023-12-04 15:30:36 +0100

    added flag widget in post

--- src/Posts/Post.jsx
+++ src/Posts/Post.jsx
@@ -345,6 +345,16 @@ return (
                     url: postUrl,
                   }}
                 />
+                <Widget
+                  src="${REPL_ACCOUNT}/widget/FlagButton"
+                  props={{
+                    item,
+                    disabled: !context.accountId || context.accountId === accountId,
+                    onFlag: () => {
+                      State.update({ hasBeenFlagged: true });
+                        },
+                    }}
+                />                
               </Actions>
             )}
             {state.showReply && (

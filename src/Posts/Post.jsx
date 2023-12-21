5d6eacebbd9474fa5eb5f7445b8b333512ea4f54
Valmir <valmir.haxholli@gmail.com>
2023-12-21 17:23:29 +0100

    NEAR-879 FIX: Updated z-index for radix popover content due to underlying issues

--- src/Posts/Post.jsx
+++ src/Posts/Post.jsx
@@ -107,7 +107,9 @@ query IndexerQuery {
 
 const Post = styled.div`
   position: relative;
-
+  div[data-radix-popper-content-wrapper] {
+    z-index: 99999 !important;  //to override the inline popover auto index
+  }
   &::before {
     content: "";
     display: block;

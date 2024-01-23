71de38e306c83ec840d22c054b2dfe5b46143ee5
Kevin Zhang <42101107+Kevin101Zhang@users.noreply.github.com>
2024-01-23 14:15:48 -0500

    feat: updated components.sql to capture stars/forks and additional info (#619)
    * feat: updated components.sql to capture stars/forks and additional info
    
    created a new table "info" to capture forks, stars, and additional metadata for components
    
    * feat: created a new table and updated indexer to capture stars/fork for components.js (#621)
    
    [Epic] Component Discovery Feed Interface #19
    
    * changed table name to metadata for clarity
    
    * renamed var to match dataplatform_near"
    
    * added latest base64decode & added additional checks for componentStarTransaction
    
    * changed to blockheight to type bigInt

--- indexers/components/components.sql
+++ indexers/components/components.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
   "versions" (
-    "id" SERIAL,
+    "id" SERIAL PRIMARY KEY,
     "block_height" BIGINT NOT NULL,
     "block_timestamp_ms" BIGINT NOT NULL,
     "code" VARCHAR NOT NULL,
@@ -8,15 +8,26 @@ CREATE TABLE
     "component_name" VARCHAR NOT NULL,
     "lines_added" INT NOT NULL,
     "lines_removed" INT NOT NULL,
-    "receipt_id" VARCHAR NOT NULL,
-    "name" VARCHAR,
+    "receipt_id" VARCHAR NOT NULL
+  );
+
+CREATE TABLE 
+  "metadata" (
+    "component_id" VARCHAR PRIMARY KEY,
+    "block_height" BIGINT NOT NULL,
+    "block_timestamp_ms" BIGINT NOT NULL,
+    "code" VARCHAR NOT NULL,
+    "component_author_id" VARCHAR NOT NULL,
+    "component_name" VARCHAR NOT NULL,
+    "star_count" INT NOT NULL,
+    "fork_count" INT NOT NULL,
+    "name" VARCHAR, 
     "image_ipfs_cid" VARCHAR,
     "description" VARCHAR,
     "fork_of_source" VARCHAR,
     "fork_of_block_height" BIGINT,
     "tags" VARCHAR,
-    "website" VARCHAR,
-    PRIMARY KEY ("id")
+    "website" VARCHAR
   );
 
 CREATE UNIQUE INDEX idx_versions_unique_receipt_version ON versions (receipt_id, component_author_id, component_name);
@@ -28,4 +39,7 @@ CREATE INDEX
   idx_versions_block_height ON versions (block_height);
 
 CREATE INDEX
-  idx_versions_fork_of ON versions (fork_of_source);
+  idx_info_fork_of ON metadata (fork_of_source);
+
+CREATE INDEX
+  idx_info_component_author_id ON metadata (component_author_id);

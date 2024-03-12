e13547ef54a37bc79dd975d211e64280723d5792
Dmitriy <sheleg.dmitriy@gmail.com>
2024-03-12 22:16:22 +0100

    chore: one more update of indexer code

--- indexers/notifications/notifications.js
+++ indexers/notifications/notifications.js
@@ -13,55 +13,76 @@ import { Block } from "@near-lake/primitives";
  * @param {block} Block - A Near Protocol Block
  */
 async function getBlock(block: Block) {
-  const nearDBIndexUpdates = block
-    .actions()
-    .filter((action) => action.receiverId === "social.near")
-    .flatMap((action) =>
-      action.operations
-        .map((operation) => operation["FunctionCall"])
-        .filter((operation) => operation?.methodName === "set")
-        .map((functionCallOperation) => ({
-          ...functionCallOperation,
-          args: base64decode(functionCallOperation.args),
-          receiptId: action.receiptId,
-        }))
-        .filter((functionCall) => {
-          if (
-            !functionCall ||
-            !functionCall.args ||
-            !functionCall.args.data ||
-            !Object.keys(functionCall.args.data) ||
-            !Object.keys(functionCall.args.data)[0]
-          ) {
-            console.log(
-              "Set operation did not have arg data in expected format"
-            );
-            return;
-          }
-          const accountId = Object.keys(functionCall.args.data)[0];
-          if (!functionCall.args.data[accountId]) {
-            console.log(
-              "Set operation did not have arg data in expected format"
-            );
-            return;
-          }
-          return Object.keys(functionCall.args.data[accountId]).includes(
-            "index"
-          );
-        })
+  const SOCIAL_DB = "social.near";
+
+  let nearDBIndexUpdates = [];
+
+  try {
+    const actions = block.actions();
+
+    if (!actions) {
+      console.log("Block has no actions");
+      return;
+    }
+    const contractActions = actions.filter(
+      (action) => action.receiverId === SOCIAL_DB
     );
+    if (!contractActions) {
+      console.log("Block has no actions");
+      return;
+    }
+    nearDBIndexUpdates = contractActions
+      .flatMap((action) =>
+        action.operations
+          .map((operation) => operation["FunctionCall"])
+          .filter((operation) => operation?.methodName === "set")
+          .map((functionCallOperation) => ({
+            ...functionCallOperation,
+            args: base64decode(functionCallOperation.args),
+            receiptId: action.receiptId,
+          }))
+          .filter((functionCall) => {
+            try {
+              if (
+                !functionCall ||
+                !functionCall.args ||
+                !functionCall.args.data ||
+                !Object.keys(functionCall.args.data) ||
+                !Object.keys(functionCall.args.data)[0]
+              ) {
+                console.log(
+                  "Set operation did not have arg data in expected format"
+                );
+                return;
+              }
+              const accountId = Object.keys(functionCall.args.data)[0];
+              if (!functionCall.args.data[accountId]) {
+                console.log(
+                  "Set operation did not have arg data in expected format"
+                );
+                return;
+              }
+              return Object.keys(functionCall.args.data[accountId]).includes(
+                "index"
+              );
+            } catch (error) {
+              console.log("Error parsing social args", functionCall);
+            }
+          })
+      );
+  } catch (error) {
+    console.log("Error parsing social operations", block.actions());
+  }
 
   if (nearDBIndexUpdates.length > 0) {
-    console.log("Found Index keys in Block...");
-    const blockHeight = block.blockHeight + "";
+    const blockHeight = block.blockHeight;
     await Promise.all(
       nearDBIndexUpdates.map(async (writeAction) => {
         const accountId = Object.keys(writeAction.args.data)[0];
-        console.log(`ACCOUNT_ID: ${accountId}`);
 
         if (Object.keys(writeAction.args.data[accountId]).includes("index")) {
           if (!writeAction.args.data[accountId].index) {
-            console.log("Wrong notify format, index is null");
+            console.log(`Wrong notify format for receiver: ${accountId}, index is null`);
             return;
           }
           if (
@@ -69,7 +90,7 @@ async function getBlock(block: Block) {
               "notify"
             )
           ) {
-            console.log("handling notify");
+            console.log(`ACCOUNT_ID: ${accountId}`);
             await handleNotify(
               accountId,
               blockHeight,
@@ -91,7 +112,6 @@ async function getBlock(block: Block) {
     try {
       let data = JSON.parse(notifyPayload);
       data = Array.isArray(data) ? data : [data];
-      console.log(data);
       const mutationPayload = data.map((notification) => {
         const { key, value } = notification;
         const {
@@ -117,9 +137,6 @@ async function getBlock(block: Block) {
         };
       });
 
-      console.log(`handling notification triggered by ${accountId}`);
-
-      //send graphql mutation
       await context.db.Notifications.upsert(
         mutationPayload,
         ["receiptId"],
@@ -134,9 +151,9 @@ async function getBlock(block: Block) {
           "actionAtBlockHeight",
         ]
       );
-      console.log("Successfully stored Notify record");
+      console.log(`Successfully stored Notify record for ${accountId}`);
     } catch (e) {
-      console.log("Failed to store Notify record");
+      console.log(`Failed to store Notify record for ${accountId}. Error: ${e}`);
     }
   }
 

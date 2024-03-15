0039dfd977254317a29b517e79577fd7fcae79b3
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2024-03-15 21:25:46 +0200

    feat: use `accounts` indexer for notifications (#642)
    * feat: use `accounts` indexer for notifications
    
    * chore: add `relationship.json` file
    
    * chore: add limit
    
    * chore: load more items
    
    * chore: cleanup
    
    * chore: remove utils
    
    * fix: load query
    
    * fix: cleanup
    
    * chore: update indexer code
    
    * chore: one more update of indexer code
    
    * chore: move back to use graphql mutations
    
    * fix: fix query ordering

--- src/NearOrg/Notifications/NotificationsList.jsx
+++ src/NearOrg/Notifications/NotificationsList.jsx
@@ -1,14 +1,8 @@
-let { manageNotification, permission, showLimit } = props;
+let { fetchNotifications, shouldFallback, manageNotification, permission, showLimit } = props;
+// showLimit means to fetch notificaitions for the preview and not the full list
 
-const index = {
-  action: "notify",
-  key: context.accountId,
-  options: {
-    limit: showLimit ?? 10,
-    order: "desc",
-    subscribe: true,
-  },
-};
+const [notifications, setNotifications] = useState(notifications ?? []);
+const [notificationsCount, setNotificationsCount] = useState(notificationsCount ?? 0);
 
 const renderItem = (item, i) => {
   if (i === 0) {
@@ -24,16 +18,73 @@ const renderItem = (item, i) => {
   );
 };
 
-return (
-  <Widget
-    src="${REPL_ACCOUNT}/widget/IndexFeed"
-    props={{
-      index,
-      manual: !!showLimit,
-      renderItem,
-      nextLimit: 10,
-      initialRenderLimit: showLimit ?? 10,
-      moderatorAccount: "${REPL_MODERATOR}",
-    }}
-  />
-);
+const NotificationsListFromChain = () => {
+  const index = {
+    action: "notify",
+    key: context.accountId,
+    options: {
+      limit: showLimit ?? 10,
+      order: "desc",
+      subscribe: true,
+    },
+  };
+
+  return (
+    <Widget
+      src="${REPL_ACCOUNT}/widget/IndexFeed"
+      props={{
+        index,
+        manual: !!showLimit,
+        renderItem,
+        nextLimit: 10,
+        initialRenderLimit: showLimit ?? 10,
+        moderatorAccount: "${REPL_MODERATOR}",
+      }}
+    />
+  );
+};
+
+const saveData = (newNotifications, notificationsTotal) => {
+  if (notificationsCount !== notificationsTotal) {
+    setNotificationsCount(notificationsTotal);
+  }
+  setNotifications([...notifications, ...newNotifications]);
+};
+
+const fetchData = (offset, limit) => {
+  if (notifications.length > 0 && showLimit) return;
+  fetchNotifications(offset, limit, saveData);
+};
+
+useEffect(() => {
+  if (shouldFallback) {
+    return;
+  }
+  fetchData(notifications.length, showLimit ?? 10);
+  () => {
+    setNotifications([]);
+  };
+}, []);
+
+const NotificationsFromGraphQL = () => {
+  return (
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={() => {
+        fetchData(notifications.length, showLimit ?? 10);
+      }}
+      hasMore={!showLimit && notifications.length < notificationsCount}
+      initialLoad={false}
+      loader={
+        <div className="loader">
+          <span className="spinner-grow spinner-grow-sm me-1" role="status" aria-hidden="true" />
+          Loading ...
+        </div>
+      }
+    >
+      {notifications.map(renderItem)}
+    </InfiniteScroll>
+  );
+};
+
+return <>{shouldFallback ? <NotificationsListFromChain /> : <NotificationsFromGraphQL />}</>;

49c350cab456db07be3c4db1d301303f20a34230
Caleb Jacob <thecalebjacob@gmail.com>
2024-03-18 14:43:00 -0600

    Adjusting SmallScreenHeader for mobile layout

--- src/Navigation/SmallScreenHeader.jsx
+++ src/Navigation/SmallScreenHeader.jsx
@@ -2,25 +2,43 @@ let { className, wrapperStyles, availableStorage, withdrawTokens, logOut } = pro
 
 const Wrapper = styled.div`
   display: flex;
-  gap: 20px;
+  gap: 16px;
   align-items: center;
+  height: 100%;
+`;
+
+const Item = styled.div`
+  display: flex;
+  width: 40px;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
 `;
 
 return (
   <Wrapper className={className} style={{ ...wrapperStyles }}>
-    <Widget
-      src="${REPL_ACCOUNT}/widget/DIG.Button"
-      props={{
-        icon: "ph ph-magnifying-glass",
-        variant: "secondary",
-        style: { width: "40px", height: "40px" },
-        href: "/${REPL_ACCOUNT}/widget/Search.IndexPage",
-      }}
-    />
-    <Widget src="${REPL_ACCOUNT}/widget/NearOrg.Notifications.NotificationButton" props={{ mobileView }} />
-    <Widget
-      src="${REPL_ACCOUNT}/widget/Navigation.ProfileDropdown"
-      props={{ availableStorage, withdrawTokens, logOut }}
-    />
+    <Item>
+      <Widget
+        src="${REPL_ACCOUNT}/widget/DIG.Button"
+        props={{
+          icon: "ph ph-magnifying-glass",
+          variant: "secondary",
+          style: { width: "40px", height: "40px" },
+          href: "/${REPL_ACCOUNT}/widget/Search.IndexPage",
+        }}
+      />
+    </Item>
+    <Item>
+      <Widget
+        src="${REPL_ACCOUNT}/widget/NearOrg.Notifications.NotificationButton"
+        props={{ size: "40px", mobileView: true }}
+      />
+    </Item>
+    <Item>
+      <Widget
+        src="${REPL_ACCOUNT}/widget/Navigation.ProfileDropdown"
+        props={{ availableStorage, withdrawTokens, logOut }}
+      />
+    </Item>
   </Wrapper>
 );

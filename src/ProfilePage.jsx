fe6442d56554e3f901f2bb69f1dd88248fbfcd8c
jackson@near.org <jacksonnear.org@jacksonnearorgs-MacBook-Pro.local>
2024-03-25 00:19:52 -0700

    feat: add blog to user profile

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -229,6 +229,10 @@ return (
           <TabsButton href={`${accountUrl}&tab=followers`} selected={state.selectedTab === "followers"}>
             Followers
           </TabsButton>
+
+          <TabsButton href={`${accountUrl}&tab=blog`} selected={state.selectedTab === "blog"}>
+            Blog
+          </TabsButton>
         </Tabs>
 
         {state.selectedTab === "overview" && (
@@ -281,6 +285,15 @@ return (
               accountId,
               components: starredComponents,
               noDataText: "This account hasn't starred any components yet.",
+            }}
+          />
+        )}
+
+        {state.selectedTab === "blog" && (
+          <Widget
+            src="${REPL_ACCOUNT}/widget/Blog.Feed"
+            props={{
+              contributors: [accountId],
             }}
           />
         )}

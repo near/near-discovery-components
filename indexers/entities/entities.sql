1741292657e34659a8196702ad2de28d7c846668
Gabe Hamilton <gabe@near.org>
2024-04-18 09:03:52 -0600

    Entities indexer tags (#768)
    * fix: entities indexer handles empty namespace key
    
    * feat: entities indexer supports tags
    
    * chore: restored export

--- indexers/entities/entities.sql
+++ indexers/entities/entities.sql
@@ -12,13 +12,22 @@ CREATE TABLE "entities"
     "logo_url"     TEXT,
     "attributes"   JSONB,
     "stars"        integer, -- default 0
+    "tags" text[],
     -- "created_at"   timestamp with time zone default now(),
     -- "updated_at"   timestamp with time zone default now(),
     PRIMARY KEY ("entity_type", "account_id", "name")
 );
+
+CREATE INDEX
+    idx_tags_array ON entities USING GIN (tags);
 
 -- CREATE TRIGGER set_dataplatform_near_agents_entities_updated_at
 --     BEFORE UPDATE
 --     ON dataplatform_near_agents.entities
 --     FOR EACH ROW
 -- EXECUTE FUNCTION dataplatform_near_agents.set_current_timestamp_updated_at();
+
+-- CREATE VIEW dataplatform_near_entities."tags" as
+-- select namespace, entity_type, unnest(tags) as tag, count(*)
+-- from dataplatform_near_entities.entities
+-- group by namespace, entity_type, tag
